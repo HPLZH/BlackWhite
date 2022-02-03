a55762672454216c69e35551ebb45fe0045f096a
HPLZH <homepig.lzh@qq.com>
2022-02-03 13:38:22 +0800

    添加生成功能

--- docs/BW_Web/BW.js
+++ docs/BW_Web/BW.js
@@ -14,12 +14,16 @@ function initGame(size){
 
 function buttonClick(r,c){
     //alert(gameSize);
+    blockClick(r,c);
+    MakeHash();
+}
+
+function blockClick(r,c){
     tryChange(r,c);
     tryChange(r+1,c);
     tryChange(r-1,c);
     tryChange(r,c+1);
     tryChange(r,c-1);
-    MakeHash();
 }
 
 function tryChange(r,c){
@@ -34,4 +38,35 @@ function tryChange(r,c){
 function setBlock(r,c,value){
     SetColor(r,c,value);
     blocks[r][c]=value;
+}
+
+function randomClick(){
+    //alert(0)
+    for(ir=0;ir<gameSize;ir++){
+        for(ic=0;ic<gameSize;ic++){
+            if(Math.random()<0.5){
+                blockClick(ir,ic);
+            }
+        }
+    }
+    MakeHash();
+}
+
+function randomSet(){
+    for(ir=0;ir<gameSize;ir++){
+        for(ic=0;ic<gameSize;ic++){
+            setBlock(ir,ic,Math.random()<0.5);
+        }
+    }
+    MakeHash();
+}
+
+function clearGame(){
+    //alert(0)
+    for(ir=0;ir<gameSize;ir++){
+        for(ic=0;ic<gameSize;ic++){
+            setBlock(ir,ic,false);
+        }
+    }
+    MakeHash();
 }
