bf995877528937f2cc6997e9ac8c9facf3c7477f
LZH <homepig.lzh@qq.com>
2022-08-08 17:02:19 +0800

    完成统计类

--- BlackWhite.App/BlackWhite.App/Statistics.cs
+++ BlackWhite.App/BlackWhite.App/Statistics.cs
@@ -15,41 +15,151 @@ namespace BlackWhite.App
         const string CLICK = "Click";
         const string GAME = "Game";
         const string WIN = "Win";
+        const string TIME = "Time";
 
+        static string[] MODE = { NORMAL, PERFECT, TIMED };
         public static void Clear()
         {
-
+            for(int i = MIN_SIZE; i <= MAX_SIZE; i++)
+            {
+                foreach(string mode in MODE)
+                {
+                    PropertiesIO.Set<ulong>(0, STATISTICS, mode, i.ToString(), CLICK);
+                    PropertiesIO.Set<ulong>(0, STATISTICS, mode, i.ToString(), GAME);
+                    PropertiesIO.Set<ulong>(0, STATISTICS, mode, i.ToString(), WIN);
+                }
+                PropertiesIO.Set<ulong>(0, STATISTICS, TIMED, i.ToString(), TIME);
+            }
         }
 
-        const string ALL = "All";
         public static class All
         {
+            public static ulong Clicks() => Normal.Clicks() + Perfect.Clicks() + Timed.Clicks();
+            public static ulong Games() => Normal.Games() + Perfect.Games() + Timed.Games();
+            public static ulong Wins() => Normal.Wins() + Perfect.Wins() + Timed.Wins();
 
+            public static ulong Clicks(int size) => Normal.Clicks(size) + Perfect.Clicks(size) + Timed.Clicks(size);
+            public static ulong Games(int size) => Normal.Games(size) + Perfect.Games(size) + Timed.Games(size);
+            public static ulong Wins(int size) => Normal.Wins(size) + Perfect.Wins(size) + Timed.Wins(size);
         }
 
         const string NORMAL = "Normal";
         public static class Normal
         {
+            //存储
             public static ulong Clicks(int size) => Get<ulong>(STATISTICS, NORMAL, size.ToString(), CLICK);
             public static void AddClick(int size) => Set<ulong>(Clicks(size) + 1, STATISTICS, NORMAL, size.ToString(), CLICK);
+            public static ulong Clicks()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Clicks(i);
+                return sum;
+            }
 
             public static ulong Games(int size) => Get<ulong>(STATISTICS, NORMAL, size.ToString(), GAME);
             public static void AddGame(int size) => Set<ulong>(Games(size) + 1, STATISTICS, NORMAL, size.ToString(), GAME);
+            public static ulong Games()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Games(i);
+                return sum;
+            }
 
             public static ulong Wins(int size) => Get<ulong>(STATISTICS, NORMAL, size.ToString(), WIN);
             public static void AddWin(int size) => Set<ulong>(Wins(size) + 1, STATISTICS, NORMAL, size.ToString(), WIN);
+            public static ulong Wins()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Wins(i);
+                return sum;
+            }
+
+            //计算
+            public static decimal WinPercentage(int size) => (decimal)Wins(size) / (decimal)Games(size);
+            public static decimal AverageClicks(int size) => (decimal)Clicks(size) / (decimal)Games(size);
         }
 
         const string PERFECT = "Perfect";
         public static class Perfect
         {
+            //存储
+            public static ulong Clicks(int size) => Get<ulong>(STATISTICS, PERFECT, size.ToString(), CLICK);
+            public static void AddClick(int size) => Set<ulong>(Clicks(size) + 1, STATISTICS, PERFECT, size.ToString(), CLICK);
+            public static ulong Clicks()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Clicks(i);
+                return sum;
+            }
 
+            public static ulong Games(int size) => Get<ulong>(STATISTICS, PERFECT, size.ToString(), GAME);
+            public static void AddGame(int size) => Set<ulong>(Games(size) + 1, STATISTICS, PERFECT, size.ToString(), GAME);
+            public static ulong Games()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Games(i);
+                return sum;
+            }
+
+            public static ulong Wins(int size) => Get<ulong>(STATISTICS, PERFECT, size.ToString(), WIN);
+            public static void AddWin(int size) => Set<ulong>(Wins(size) + 1, STATISTICS, PERFECT, size.ToString(), WIN);
+            public static ulong Wins()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Wins(i);
+                return sum;
+            }
+
+            //计算
+            public static decimal WinPercentage(int size) => (decimal)Wins(size) / (decimal)Games(size);
+            public static decimal AverageClicks(int size) => (decimal)Clicks(size) / (decimal)Games(size);
         }
 
         const string TIMED = "Timed";
         public static class Timed
         {
+            //存储
+            //Clicks不显示但是参与总数统计
+            public static ulong Clicks(int size) => Get<ulong>(STATISTICS, TIMED, size.ToString(), CLICK);
+            public static void AddClick(int size) => Set<ulong>(Clicks(size) + 1, STATISTICS, TIMED, size.ToString(), CLICK);
+            public static ulong Clicks()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Clicks(i);
+                return sum;
+            }
 
+            //以秒为单位
+            public static ulong Time(int size) => Get<ulong>(STATISTICS, TIMED, size.ToString(), TIME);
+            public static void AddSecond(int size) => Set<ulong>(Time(size) + 1, STATISTICS, TIMED, size.ToString(), TIME);
+            public static ulong Time()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Time(i);
+                return sum;
+            }
+
+            public static ulong Games(int size) => Get<ulong>(STATISTICS, TIMED, size.ToString(), GAME);
+            public static void AddGame(int size) => Set<ulong>(Games(size) + 1, STATISTICS, TIMED, size.ToString(), GAME);
+            public static ulong Games()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Games(i);
+                return sum;
+            }
+
+            public static ulong Wins(int size) => Get<ulong>(STATISTICS, TIMED, size.ToString(), WIN);
+            public static void AddWin(int size) => Set<ulong>(Wins(size) + 1, STATISTICS, TIMED, size.ToString(), WIN);
+            public static ulong Wins()
+            {
+                ulong sum = 0;
+                for (int i = MIN_SIZE; i <= MAX_SIZE; i++) sum += Wins(i);
+                return sum;
+            }
+
+            //计算
+            public static decimal WinPercentage(int size) => (decimal)Wins(size) / (decimal)Games(size);
+            public static decimal AverageTime(int size) => (decimal)Time(size) / (decimal)Games();
         }
     }
 }
